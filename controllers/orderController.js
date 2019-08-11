8aba50a7ebb3e04c0dbd4b0ee581c59630734e9f
Amina Rahimli <emine@rehimli.info>
2019-08-12 01:08:46 +0300

    add order partly ficed

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -25,16 +25,28 @@ export const getOrderById = async (req, res, next) => {
 export const addNewOrder = async (req, res, next) => {
   try {
     const { body } = req;
-    const { sum, userId, createdAt } = body;
+    const { productIds, userId } = body;
 
-    const sql = `insert into \`order\` set ?`;
-    const data = await makeQuery(sql, {
-      sum,
+    // Why this query returns only 1 value, when 'sql where in' should return all matching?
+    const selectProducts = 'select * from product where id in (?)';
+    const products = await makeQuery(selectProducts, productIds);
+
+    const orderSum = products.reduce((sum, product) => sum += product.price, 0);
+
+    const insertOrder = `insert into \`order\` set ?`;
+    const orderData = await makeQuery(insertOrder, {
+      sum: orderSum,
       userId,
-      createdAt,
+      createdAt: new Date(),
     });
 
-    res.status(201).send(data);
+    // Why this query inserts only 1 row, when values are multiple?
+    const insertOrderProducts = 'insert into product_in_order(productid, orderid) values (?)';
+    const values = productIds.map(id => [id, orderData.insertId]);
+
+    await makeQuery(insertOrderProducts, values);
+
+    res.status(201).send(orderData);
   } catch (error) {
     next(new AppError(error.mesage));
   }
