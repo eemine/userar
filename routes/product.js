f8cc9f588af949b5ee3aed37a97b28c0fce2b5c9
Amina Rahimli <emine@rehimli.info>
2019-08-12 08:28:04 +0300

    delete product functionality added

--- routes/product.js
+++ routes/product.js
@@ -1,12 +1,22 @@
 import express from 'express';
-import { getProductById, indexAction, modifyProduct, deleteProduct } from '../controllers/productController';
+import {
+  getProductById,
+  indexAction,
+  modifyProduct,
+  deleteProduct,
+} from '../controllers/productController';
 
 const router = express.Router();
+
+// GET - is used to get data
+// POST - is used to add new data to table
+// PUT - is used to modify/edit existing data in table
+// DELETE - is used to remove/delete data from table
 
 router.get('/', indexAction);
 router.post('/', modifyProduct);
 router.put('/:productId', modifyProduct)
 router.get('/:productId', getProductById);
-router.get('/', deleteProduct);
+router.delete('/:productId', deleteProduct);
 
 export default router;
